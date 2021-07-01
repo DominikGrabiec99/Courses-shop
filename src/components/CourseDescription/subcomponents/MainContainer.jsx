23e76edd5c60c700d6ae21123ccc9818f0fe4b2f
Dominik Grabiec <dominik.grabiec@monaco.net.pl>
2021-07-01 13:01:25 +0200

    style courseDescription

--- src/components/CourseDescription/subcomponents/MainContainer.jsx
+++ src/components/CourseDescription/subcomponents/MainContainer.jsx
@@ -3,31 +3,40 @@ import bemCssModules from 'bem-css-modules'
 import { default as CourseDescriptionStyles } from '../CourseDescription.module.scss';
 
 const block = bemCssModules(CourseDescriptionStyles);
-const MainContainer = ({description, knowledge}) => {
+const MainContainer = ({description, knowledge, title, shortDiscription}) => {
 
   let allKnowledges =null;
   let descriptionCourse =null;
 
   if(knowledge){
     allKnowledges = knowledge.map(knowledge => (
-      <p key={knowledge}>{knowledge}</p>
+      <p key={knowledge} className={block('pargInformation')}><i className={"fas fa-check " + block('check')}></i>{knowledge}</p>
     ))
 
-    descriptionCourse = description.map(description => (
-      <p key={description}>{description}</p>
-    ))
+    descriptionCourse = description.map(description => {
+      const classWeigth = description.length < 80 ? 'pargDescriptionWeigth' : ''
+      return (
+        <p key={description} className={block('pargDescription') + " " + block(classWeigth)}>{description}</p>
+    )})
   }
   return ( 
     <div className={block('main-container')}>
+      <article className={block('absoluteBlock')}>
+        <div className={block('boxHeader')}>
+          <h1 className={block('header-title')}>{title}</h1>
+          <p className={block('header-shortDiscription')}>{shortDiscription}</p>
+        </div>
+      </article>
         <article className={block('main-info')}>
-          <section>
-            <h3>Czego się nauczysz</h3>
-            {allKnowledges}
+          <section className={block('toLearn')}>
+            <h3 className={block('titleToLearn')}>Czego się nauczysz</h3>
+            <div className={block('boxInformation')}>
+              {allKnowledges}
+            </div>
           </section>
-
-          <section>
-            <h3>Opis</h3>
-            <div>
+          <section className={block('description-info')}>
+            <h3 className={block('titleToDescription')}>Opis</h3>
+            <div className={block('box-description')}>
               {descriptionCourse}
             </div>
           </section>
